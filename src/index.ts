6c71f5bbcc645eaf446b202209de0511e3a11d2a
Mohammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2024-01-08 20:53:16 +0600

    connect with git repo

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const PORT = 5000;
+const app = express();
+
+/* Extend the Request interface in place to include your custom variable */
+// declare global {
+//     namespace Express {
+//         interface Request {
+
+//         }
+//         interface Response {
+
+//         }
+//     }
+// }
+
+// use middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req: Request, res: Response) => {
+    res.status(200).send({
+        status: 200,
+        message: `Server is running successfully at : ${PORT}`,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running in : http://localhost:${PORT}`);
+});
