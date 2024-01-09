b8f77b21208ba5dd9a1a15e0dadb5b127697be42
Mohammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2024-01-09 22:33:13 +0600

    mna-node-app v1.0.0 published

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 
-const {execSync} = require("child_process");
+const { execSync } = require("child_process");
 
-const runCommand = (command)=> {
-    try {
-      execSync(`${command}`,{stdio: 'inherit'});
-    } catch (err) {
-      console.log(`Failed to execute ${command}`,err);
-      return false;
-    }
-    return true;
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (err) {
+    console.log(`Failed to execute ${command}`, err);
+    return false;
+  }
+  return true;
 }
 
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/Mohammad-Nafis-Abdullah/mna-node-server.git ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
+const creatingFolderCommand = `cd ${repoName} && rm -rf ./bin && rm -rf ./.git && cd ./src && mkdir db && mkdir middlewares && mkdir routes && mkdir schema && mkdir utils`;
+const openInVsCodeCommand = `cd ${repoName} && code .`;
 
 
 console.log(`Cloning the repository with name ${repoName}`);
@@ -30,5 +32,15 @@ if (!installDeps) {
   process.exit(-1);
 }
 
-console.log(`Congratulations! You are ready. Follow the following commands to start`);
-console.log(`cd ${repoName} && code . && npm run dev`);
+const creatingFolder = runCommand(creatingFolderCommand);
+if (!creatingFolder) {
+  process.exit(-1);
+}
+
+console.log(`Congratulations! You are ready.`);
+console.log(`Enter "npm run dev" to run the server in development`);
+
+const openInVsCode = runCommand(openInVsCodeCommand);
+if (!openInVsCode) {
+  process.exit(-1);
+}
